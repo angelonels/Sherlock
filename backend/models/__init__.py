bcba771b4df821da5e906c8d1ac892b47e3e5240
Angelo Nelson <angelonelson.dev@gmail.com>
2026-04-11 15:52:44 +0530

    refactor: export Dataset and AnalysisRun from models package

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,20 @@
 # Expose the Base and our models here so Alembic and our database schema registry
 # can easily discover them all at once during migrations.
 from database import Base
-from .user import User
+from .analysis import AnalysisRun, ChatMessage, QueryAttempt
 from .chat import ChatSession
+from .dataset import Dataset, DatasetColumn, DatasetQualityIssue, UploadSession
+from .user import User
 
-__all__ = ["Base", "User", "ChatSession"]
+__all__ = [
+    "AnalysisRun",
+    "Base",
+    "ChatMessage",
+    "ChatSession",
+    "Dataset",
+    "DatasetColumn",
+    "DatasetQualityIssue",
+    "QueryAttempt",
+    "UploadSession",
+    "User",
+]
